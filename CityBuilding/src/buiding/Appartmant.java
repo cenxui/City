bc4352a96d21faa33a47e20045e040e4501b5f81
cenxui lin <b97201017@ntu.edu.tw>
2016-07-08 15:30:40 +0800

    initial CityBuilding project

---
+++ CityBuilding/src/buiding/Appartmant.java
@@ -0,0 +1,13 @@
+package buiding;
+
+import building.Building;
+import building.residence.ResidentBuilding;
+
+public class Appartmant extends ResidentBuilding {
+
+	public Appartmant(Building building) {
+		super(building);
+	
+	}
+
+}
