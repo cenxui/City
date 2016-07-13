b9b8445b3db2b9ffb441697a0c40e6206267b5f0
Hua Ying Huang <b00107012@ntu.edu.tw>
2016-07-13 11:22:08 +0800

    add bedroom interface

---
+++ City/src/building/Bedroom.java
@@ -0,0 +1,19 @@
+package building;
+
+interface Bedroom {
+	public static final int KingBed = 3;
+	public static final int QueenBed = 2;
+	public static final int TwinBed = 1;
+	public static final int FutonBed = 0;
+	
+	boolean isRedecoratable();
+	boolean hasBedroom();
+	boolean hasDresser();
+	boolean hasWardrodbe();
+	boolean hasMirror();
+	int getBedroomNumber();
+	void setBedType(int bedType);
+	int getBedType();
+	void setBedNumber(int number);
+	int getBedNumber();
+}
