f3dc3759ba3d6f2113303b7b9d791420bc868d12
Hua Ying Huang <b00107012@ntu.edu.tw>
2016-07-20 17:49:25 +0800

    initial class Kitchen.java

---
+++ City/src/building/residence/Kitchen.java
@@ -0,0 +1,12 @@
+package building.residence;
+
+interface Kitchen {
+	boolean hasKitchen();
+	boolean hasStove();
+	boolean hasFridge();
+	boolean hasOven();
+	boolean hasPercolator();
+	
+	int getStoveNumber();
+
+}
