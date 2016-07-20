a45ddd8c84c530587535d77310e437bedf6234b0
Hua Ying Huang <b00107012@ntu.edu.tw>
2016-07-20 17:49:54 +0800

    initial class Lavatory.java

---
+++ City/src/building/residence/Lavatory.java
@@ -0,0 +1,12 @@
+package building.residence;
+
+interface Lavatory {
+	boolean hasLavatory();
+	boolean hasToilet();
+	boolean hasUrinal();
+	boolean hasSink();
+	boolean hasMirror();
+	boolean hasTowelRack();
+	boolean hasCabinet();
+	
+}
