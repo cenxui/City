9a3be100129d3573b3d223d7cf9438b3365527d6
Hua Ying Huang <b00107012@ntu.edu.tw>
2016-07-26 16:59:07 +0800

    initial RecreationRoom

---
+++ City/src/building/residence/RecreationRoom.java
@@ -0,0 +1,11 @@
+package building.residence;
+
+import building.Room;
+
+interface RecreationRoom extends Room {
+	boolean hasRecreationRoom();
+	boolean hasKTVEquipment();
+	boolean hasProjector();
+	boolean hasStereo();
+
+}
