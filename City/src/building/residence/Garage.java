5e18f6697b90ebf7415c82bea1a2558d58a11bcd
Hua Ying Huang <b00107012@ntu.edu.tw>
2016-07-27 16:40:11 +0800

    initial Garage

---
+++ City/src/building/residence/Garage.java
@@ -0,0 +1,10 @@
+package building.residence;
+
+interface Garage {
+	boolean hasGarage();
+	boolean hasCarRepairTools();
+	boolean hasGarageTools();
+	boolean hasCarJacks();
+	int getGarageSize();
+
+}
